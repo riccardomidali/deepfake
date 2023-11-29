1b6262a51616da1f1f71a4cb601d8126226248a3
riccardomidali <63498341+riccardomidali@users.noreply.github.com>
2023-11-29 17:55:29 +0100

    Add files via upload

---
+++ RealtimeRecognition.py
@@ -0,0 +1,10 @@
+#import della libreria
+from deepface import DeepFace
+
+#esegue la funzione "stream"
+DeepFace.stream("data")
+
+#aprirà la webcam e mostrerà gli attributi del volto inquadrato
+#inoltre lo confronta con quelli presenti nella cartella "database" e mostra se ci sono dei match
+
+#!! per terminare l'esecuzione digitare crtl+c sul terminale !!
